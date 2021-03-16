495a498e5f89c2ffbb8f0173c12802ef33429d14
giacomo9999 <gmarchesi888@gmail.com>
2021-03-16 14:11:26 -0400

    fetching and displaying data successfully.

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -2,20 +2,23 @@ import * as d3 from "d3";
 
 export default function D3Chart(element) {
   const data = [30, 10, 65, 42, 20, 35];
+  const url = "https://udemy-react-d3.firebaseio.com/ages.json";
   const svg = d3
     .select(element)
     .append("svg")
     .attr("width", 500)
     .attr("height", 500);
 
-  const rects = svg.selectAll("rect").data(data);
-
-  rects
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => 30 + i * 100)
-    .attr("y", 50)
-    .attr("height", (d) => d * 10)
-    .attr("width", 50)
-    .attr("fill", "yellowgreen");
+  d3.json(url).then((agesData) => {
+    console.log(agesData);
+    const rects = svg.selectAll("rect").data(agesData);
+    rects
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => 30 + i * 100)
+      .attr("y", 50)
+      .attr("height", (d) => d.age * 20)
+      .attr("width", 50)
+      .attr("fill", "yellowgreen");
+  });
 }
